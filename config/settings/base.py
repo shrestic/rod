848cb0291c2f9a52d82d1fc632c843cf3b8d9106
shrestic <nhatphongcgp@gmail.com>
2025-03-31 21:18:44 +0700

    bug(customer): fix user serialization issue

--- config/settings/base.py
+++ config/settings/base.py
@@ -363,8 +363,9 @@ BASE_DJOSER = {
     # Custom serializers for different user operations
     "SERIALIZERS": {
         "activation": "djoser.serializers.ActivationSerializer",
-        "user_create": "core.serializers.UserCreateSerializer",
-        "current_user": "core.serializers.UserSerializer",
+        "user_create": "rod.users.serializers.UserCreateSerializer",
+        "user": "rod.users.serializers.UserSerializer",
+        "current_user": "rod.users.serializers.UserSerializer",
     },
     "EMAIL": {
         "activation": "rod.email.djoser.CustomActivationEmail",
