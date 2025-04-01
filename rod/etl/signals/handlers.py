b497d36fef06b74a781f8e2fdd77572aafc52d30
shrestic <nhatphongcgp@gmail.com>
2025-04-01 22:05:46 +0700

    feat(employee): add employee model and migration

--- rod/etl/signals/handlers.py
+++ rod/etl/signals/handlers.py
@@ -8,4 +8,7 @@ from rod.etl.services import CustomerService
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_customer_for_new_user(sender, **kwargs):
     if kwargs["created"]:
-        CustomerService().customer_create(user=kwargs["instance"])
+        user = kwargs["instance"]
+        if user.is_staff:
+            return
+        CustomerService().customer_create(user=user)
