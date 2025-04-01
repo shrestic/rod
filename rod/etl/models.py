c878ed316f8114f71d4a2cb73391821323fd2599
shrestic <nhatphongcgp@gmail.com>
2025-04-01 09:51:28 +0700

    feat(customer): Add customer admin

--- rod/etl/models.py
+++ rod/etl/models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.conf import settings
+from django.contrib import admin
 from django.db import models
 
 from rod.common.models import BaseModel
@@ -24,11 +25,11 @@ class Customer(BaseModel):
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
 
-    @property
+    @admin.display(ordering="user__first_name")
     def first_name(self):
         return self.user.first_name
 
-    @property
+    @admin.display(ordering="user__last_name")
     def last_name(self):
         return self.user.last_name
 
